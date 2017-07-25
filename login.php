f48b84357c7a31cd7049d32c3b8afb59c44976cf
System Administrator <root@mac-no-MacBook-Air.local>
2017-07-25 14:13:24 +0900

    logup input

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php include_once 'temp/header.html'; ?>
+<header>
+</header>
+<main class="up">
+    <form action="#" >
+        <input class="logup" type="email" name="email" placeholder="メールアドレス" size="40">
+        </br>
+        <input class="logup" type="password" name="pass1" placeholder="パスワード" size="40">
+        </br>
+        <input class="logup" type="password" name="pass2" placeholder="パスワード再確認" size="40">
+        </br>
+        <input class="check" type="checkbox" name="kiyaku">
+        <a href="#">こちら</a>の利用規約にどうしますか？<br>
+        同意する場合はこちらにチェックを入れてください<br>
+        <input class="submit" type="reset" value="リセット">
+        <input class="submit" type="submit" value="送信">
+    </form>
+</main>
