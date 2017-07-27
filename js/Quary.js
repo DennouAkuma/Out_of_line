ffda8b5e4a8f50620a2cc38f07e2ea87c1f54914
Humanfilter <marukusu1883@gmail.com>
2017-07-27 14:38:10 +0900

    add Error message

---
+++ js/Quary.js
@@ -0,0 +1,31 @@
+$(function() {
+    $('#pas2').change(function(){
+        var pass1 = $('#pas1').val();
+        var pass2 = $('#pas2').val();
+        if(pass1 === pass2){
+
+        }else{
+
+        }
+    });
+    $('#logup').submit(function() {
+        var email = $('#mail').val();
+        var pass1 = $('#pas1').val();
+        var pass2 = $('#pas2').val();
+        if(email == ""){
+            $('.email').css('display', 'inline-block');
+            return false;
+        }else if(pass1 == ""){
+            $('.pass1').css('display', 'inline-block');
+            return false;
+        }else if(pass2 == ""){
+            return false;
+        }
+        if(pass1 === pass2){
+            this.submit();
+        }else{
+            $('.pass2').css('display', 'inline-block');
+            return false;
+        }
+    });
+});
--- logup.php
+++ logup.php
@@ -4,17 +4,35 @@ include_once 'temp/header.php'; ?>
 <header class="up">
 </header>
 <main class="up">
-    <form action="logup_sgin.php">
+    <form action="logup_sgin.php" class="logup_form" id="logup">
         <h1 class="sin_title">Welcome to Out of line!</h1>
-        <input class="logup" type="email" name="email" placeholder="メールアドレス" size="40">
+
+        <!--メールアドレス-->
+        <input id="mail" class="logup" type="email" name="email" placeholder="メールアドレス" size="40">
+        <div class="email error" id="mail_erro_i">
+            <p>これは、メールアドレスではありません</p>
+        </div>
         </br>
-        <input class="logup" type="password" name="pass1" placeholder="パスワード" size="40">
+
+        <!--パスワード-->
+        <input id="pas1" class="logup" type="password" name="pass1" placeholder="パスワード" size="40">
+        <div class="pass1 error">
+            <p>危険度</p>
+        </div>
         </br>
-        <input class="logup" type="password" name="pass2" placeholder="パスワード再確認" size="40">
-        <p class="doui"><input class="check" type="checkbox" name="kiyaku">
-        <a href="#">こちら</a>の利用規約に同意しますか？<br>
-        同意する場合はこちらにチェックを入れてください</p>
-        <input class="submit" type="reset" value="リセット">
+
+        <!--パスワード確認-->
+        <input id="pas2" class="logup" type="password" name="pass2" placeholder="パスワード再確認" size="40">
+        <div class="pass2 error">
+            <p class="midasi_1" id="pas2_midasi">パスワードが一致しません</p>
+        </div>
+
+        <!--同意-->
+        <p class="doui"><input class="check" type="checkbox" name="kiyaku" />
+        <a href="#">こちら</a>の利用規約に同意しますか？</p>
+        <p class="doui">同意する場合はこちらにチェックを入れてください</p>
+
+        <input class="submit" type="reset" value="リセット">
         <input class="submit" type="submit" value="送信">
     </form>
 </main>
